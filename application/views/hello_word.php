d26c15868fefb996ad888a293b1e0f155dea36c6
AhmadTorik <TorikTampan@gmail.com>
2019-01-21 11:15:04 +0700

    asoy

---
+++ application/views/hello_word.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>My First CI App</title>
+    <link rel="stylesheet" href="asset/style.css">
+</head>
+<body>
+    <div id="container">
+        <h1>Hello World</h1>
+        <div id="body">
+            <!-- <p>Selmat kamu sudah bisa menggunakan konsep Controller dan View pada CI</p> -->
+        </div>
+    </div>
+    <h3>Biodata Aghna</h3>
+    <div id="biodata">
+        
+        <div id="isi">
+        
+        <div id="gambar">
+            <img src="" alt="">
+        </div>
+    </div>
+</body>
+</html>
---
+++ application/views/v_keluarga.php
@@ -0,0 +1,128 @@
+<html>
+<head>
+<title></title>
+<style>
+    h1 {
+        text-align: center;
+        font-size: 70px;
+        font-family: chiller;
+        padding:50px 0px 20px 0px;
+        text-shadow: 2px 2px 2px black;
+
+      }
+</style>
+<marquee behavior="alternate" direction="up" scrollamount="3" scrolldelay="65" height="80" style="text-align" filter:wave></div>
+    <h1>My Family</h1></marquee>
+<br>
+<img src="<?= base_url('assets/image/aku.jpg')?>" style="width:100%">
+<br><br>
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2 style= "text-align:center; font-family: comic san ms; font-size: 25px; color: brown">Ayah</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/boy.png')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>Nama</td> <td>: Rohman</td>
+                            </tr>
+                            <tr>
+                                <td>Pekerjaan</td> <td>: Wiraswasta</td>
+                            </tr>
+                            <tr>
+                                <td>Umur</td> <td>: 45 tahun</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td> <td>: Tulungagung</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2 style= "text-align:center; font-family: comic san ms; font-size: 25px; color: brown">Mama</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/girl.png')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>Nama</td> <td>: Aminah</td>
+                            </tr>
+                            <tr>
+                                <td>Pekerjaan</td> <td>: Ibu Rumah Tangga</td>
+                            </tr>
+                            <tr>
+                                <td>Umur</td> <td>: 45 Tahun</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td> <td>: Tulungagung</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2 style= "text-align:center; font-family: comic san ms; font-size: 25px; color: brown">Kakak</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/girl.png')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>Nama</td> <td>: Rendyna Desika Rani</td>
+                            </tr>
+                            <tr>
+                                <td>Pekerjaan</td> <td>: Bidan</td>
+                            </tr>
+                            <tr>
+                                <td>Umur</td> <td>: 25 tahun</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td> <td>: Tulungagung</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+</head>
+
+</html>
